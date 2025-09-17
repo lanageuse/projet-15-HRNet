80bb95f535001b7096e18be73c51249f2453bfb4
Dev Lanageuse <dev@lanageuse.com>
2025-09-17 13:20:52 +0200

    feat: add React Router with lazy-loaded pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './styles/App.css'
+import { Route, Routes } from "react-router";
+import "./styles/App.css";
+import { lazy, Suspense } from "react";
 
 function App() {
+  const Home = lazy(() => import("./pages/home"));
+  const EmployeeList = lazy(() => import("./pages/employees"));
   return (
     <>
+      <Suspense fallback="loading">
+          <Routes>
+            <Route index element={<Home/>}></Route>
+            <Route path="/employees" element={<EmployeeList/>}></Route>
+          </Routes>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
