c891280921fd5fc4ef1a6da7a76f0cb1bc27ce1d
Dev Lanageuse <dev@lanageuse.com>
2025-09-17 15:40:37 +0200

    Ajout de commentaires JSDoc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Route, Routes } from "react-router";
 import "./styles/App.css";
 import { lazy, Suspense } from "react";
-
+/**
+ * Composant principal de l'application qui configure le routage.
+ * Utilisation de React Router pour gérer la navigation entre les pages.
+ * Les composants sont chargés de manière asynchrone.
+ */
 function App() {
   const Home = lazy(() => import("./pages/home"));
   const EmployeeList = lazy(() => import("./pages/employees"));
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import "./styles/index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+/**
+ * Point d'entrée principal de l'application
+ * Configure le routeur.
+ */
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
