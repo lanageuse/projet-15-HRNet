2e8c702565d3100bb3d08e55a59cce52eefc4ef4
Dev Lanageuse <dev@lanageuse.com>
2025-09-22 13:40:16 +0200

    feat: mise à jour du logo et ajustement des styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,34 @@ export const Header = () => {
     <header>
       <nav className={style.nav}>
         <Link to="/" className={style.logo}>
-          HRnet
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="100 -35 620 230"
+            width="200px"
+            height="50px"
+          >
+            <polygon
+              points="100,20 170,60 170,140 100,180 30,140 30,60"
+              fill="none"
+              stroke-width="5"
+            />
+            <circle cx="100" cy="20" r="16" />
+            <circle cx="170" cy="60" r="16" />
+            <circle cx="170" cy="140" r="16" />
+            <circle cx="100" cy="180" r="16" />
+            <circle cx="30" cy="140" r="16" />
+            <circle cx="30" cy="60" r="16" />
+            <text
+              x="60%"
+              y="50%"
+              text-anchor="middle"
+              dominant-baseline="middle"
+              fontSize="6rem"
+              fill="white"
+            >
+              HRnet
+            </text>
+          </svg>
         </Link>
         <Link to="/employees" className={style.link}>
           → View Current Employees
