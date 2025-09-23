2a5bf01cd5cae9720f09153e2105817e1978efb8
Dev Lanageuse <dev@lanageuse.com>
2025-09-23 14:16:06 +0200

    feat: Ajout du composant dropDown

---
+++ src/components/dropdown/Dropdown.tsx
@@ -0,0 +1,78 @@
+import { useCallback, useEffect, useState } from "react";
+import type { DropDownItems, DropDownOptions } from "../../types/types";
+import style from "./dropdown.module.css";
+import { useOutsideClick } from "./hooks/useOutsideClick";
+
+export const DropDown = ({
+  name,
+  id,
+  title,
+  data,
+  selectedId,
+  onSelect,
+}: DropDownOptions) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<DropDownItems | undefined>(
+    selectedId ? data?.find((item) => item.id === selectedId) : undefined
+  );
+  const ref = useOutsideClick(() => {
+    setIsOpen(false)
+  });
+
+  const handleChange = useCallback(
+    (item: DropDownItems) => {
+      onSelect && onSelect(item);
+    },
+    [onSelect]
+  );
+
+  // Mise à jour de l'item sélectionné quand les données changent
+  useEffect(() => {
+    if (selectedId && data) {
+      const newSelectedItem = data.find((item) => item.id === selectedId);
+      newSelectedItem && setSelectedItem(newSelectedItem);
+    } else {
+      setSelectedItem(undefined);
+    }
+  }, [selectedId, data]);
+
+  return (
+    <div ref={ref} className={style?.dropDownContainer}>
+      <button
+        id={id}
+        name={name}
+        aria-label="Toggle dropdown"
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+        className={style?.dropDownBtn}
+      >
+        <span>{selectedItem?.name || title}</span>
+        <span className={isOpen ? style.open : ""}> {"▾"} </span>
+      </button>
+      {isOpen && (
+        <div aria-label="Dropdown menu" className="">
+          <ul
+            role="menu"
+            aria-labelledby={id}
+            aria-orientation="vertical"
+            className={style?.dropDownList}
+          >
+            {data.map((item) => (
+              <li
+                key={item.id}
+                onClick={() => handleChange(item)}
+                className={`${style?.dropDownItem} ${
+                  selectedItem?.id === item.id ? style.selected : ""
+                }`}
+              >
+                <span>{item.name}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
