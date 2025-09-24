43ca6e312f947f71ae21380b52e7e4f30c7d8627
Dev Lanageuse <dev@lanageuse.com>
2025-09-24 10:12:30 +0200

    feat: Ajout du composant Tbody

---
+++ src/components/dataTable/UI/Tbody.tsx
@@ -0,0 +1,26 @@
+import { usePagination } from "../../pagination/hooks/usePagination";
+import style from "../dataTable.module.css";
+
+/**
+ * Composant du corps du tableau des employés
+ * Affiche les données des employés
+ * Gère l'état de chargement si aucun employé n'est disponible
+ */
+export const Tbody = () => {
+  const {currentEmployees} = usePagination()
+  return currentEmployees ? (
+    <tbody className={style.tableBody}>
+      {currentEmployees.map((employee: any, index: any) => (
+        <tr key={index} className={style.tableRow}>
+          {Object.values(employee).map((value: any, index) => (
+            <td key={index} className={style.tableCell}>
+              <div className={style.capitalize}>{value}</div>
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  ) : (
+    <div>Loading ...</div>
+  );
+};
