e4775cba52a39cc5fa200f25594d19f852ae0d21
Dev Lanageuse <dev@lanageuse.com>
2025-09-24 10:13:11 +0200

    feat: Ajout du composant  Thead

---
+++ src/components/dataTable/UI/Thead.tsx
@@ -0,0 +1,25 @@
+import { usePagination } from "../../pagination/hooks/usePagination";
+import style from "../dataTable.module.css";
+
+/**
+ * Composant d'en-tête du tableau des employés
+ * Affiche les colonnes avec possibilité de tri (fonctionnalité en développement)
+ */
+export const Thead = () => {
+  const { theadItems } = usePagination();
+  
+  return (
+    <thead className={style.thead}>
+      <tr className={style.tableRow}>
+        {Object.keys(theadItems).map((item) => (
+          <th
+            key={item}
+            className={style.tableHead}
+          >
+            {item} <span>⇅</span>
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
