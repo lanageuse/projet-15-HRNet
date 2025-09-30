a0f4928f01c121737128c4f4ca3b24f0450f71fd
Dev Lanageuse <dev@lanageuse.com>
2025-09-30 16:43:24 +0200

    feat: ajouter le fichier d'index pour centraliser les exports des composants
    - Créer src/components/index.ts pour exporter tous les composants
    - Faciliter les imports et améliorer l'organisation du code

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+
+export {Pagination} from '../components/pagination/Pagination'
+export {TableHead} from '../components/tableHead/TableHead'
+export {TableBody} from '../components/tableBody/TableBody'
+export {SearchDataTable} from '../components/search/SearchDataTable'
+export {ItemsPerPage} from './itemsPerPage/ItemsPerPage'
+export {DropDown} from './dropdown/Dropdown'
+export {EmployeeForm} from "./form/Form"
+export {Header} from "./header/Header"
