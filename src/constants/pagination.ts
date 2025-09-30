90f3bffc027bea5837dbf32e11bfb2e15681d727
Dev Lanageuse <dev@lanageuse.com>
2025-09-30 16:43:14 +0200

    refactor: réorganiser les constantes en modules séparés
    - Diviser constants.ts en form.ts et pagination.ts
    - Créer un fichier d'index pour les exports des constantes
    - Ajouter les données complètes des états américains et départements

---
+++ src/constants/pagination.ts
@@ -0,0 +1,10 @@
+/**
+ * Pagination initale
+ */
+export const ITEM_PER_PAGE = [
+  { id: "0", value: "10" },
+  { id: "1", value: "15" },
+  { id: "2", value: "20" },
+  { id: "3", value: "25" },
+  { id: "4", value: "30" },
+];
