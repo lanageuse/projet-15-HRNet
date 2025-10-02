759b02939841570d216bc177ad1eb5c605f879df
Dev Lanageuse <dev@lanageuse.com>
2025-10-02 11:06:50 +0200

    feat: ajout de la constante TABLE_HEAD_DATA pour les en-têtes
    - Création du fichier tableHead.ts
    - Définition des paires clé-valeur

---
+++ src/constants/tableHead.ts
@@ -0,0 +1,11 @@
+export const TABLE_HEAD_DATA = {
+  firstName: "First Name",
+  lastName: "Last Name",
+  birthday: "Birthday",
+  startDate: "Start Date",
+  street: "Street",
+  city: "City",
+  state: "State",
+  zipCode: "Zip Code",
+  department: "Department",
+};
